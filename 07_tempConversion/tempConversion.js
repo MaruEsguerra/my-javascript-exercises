9bdc6077e651b11c6f89b28ccb262236af43d0f9
Maru Esguerra <162283602+MaruEsguerra@users.noreply.github.com>
2025-01-27 16:58:18 +0800

    feat: Answers 07_tempConversion
    Answers and passes all the tests of the 07_tempConversion folder.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsiusConversion = (5 / 9) * (fahrenheit - 32);
+  let celsius = Math.round(celsiusConversion * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheitConversion = ((9 / 5) * celsius) + 32;
+  let fahrenheit = Math.round(fahrenheitConversion * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
