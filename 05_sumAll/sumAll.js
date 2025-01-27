4842de608be7a7fecf331af3535e3c023f9157bd
Maru Esguerra <162283602+MaruEsguerra@users.noreply.github.com>
2025-01-27 16:34:52 +0800

    feat: Answers 05_sumAll Answers and passes all the tests of the 05_sumAll folder.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    } else if (b > a) {
+        for (let i = 0; i < b; i++) {
+            sum = sum + (a + i);
+        }
+    } else {
+        for (let i = 0; i < a; i++) {
+            sum = sum + (b + i);
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
