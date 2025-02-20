cf7dec3e2f4934d329b990193dbad61e530aaa48
Maru Esguerra <162283602+MaruEsguerra@users.noreply.github.com>
2025-02-20 14:38:20 +0800

    feat: Answers 10_fibonacci Answers and passes all the tests of the 10_fibonacci folder.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (typeof n !== "number") {
+        n = parseInt(n);
+    } else if (n < 0) {
+        return "OOPS";
+    }
 
+    const fib = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+
+    return fib[n];
 };
 
 // Do not edit below this line
