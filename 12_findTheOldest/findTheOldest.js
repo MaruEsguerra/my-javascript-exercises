1bcd72b580fd5e93e508f437fc067ba0e8e1596a
Maru Esguerra <162283602+MaruEsguerra@users.noreply.github.com>
2025-02-20 15:59:59 +0800

    feat: Answers 12_findTheOldest Answers and passes all the tests of the 12_findTheOldest folder.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const person = people.sort((a, b) => {
+        if (a.yearOfDeath === undefined) {
+            a.yearOfDeath = new Date().getFullYear();
+        } else if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
 
+        const currentPerson = a.yearOfDeath - a. yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return nextPerson - currentPerson;
+    });
+    return person[0];
 };
 
 // Do not edit below this line
