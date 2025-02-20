505cf3ea1597973e5ea202a65e00a6ed545fe5cf
Maru Esguerra <162283602+MaruEsguerra@users.noreply.github.com>
2025-02-20 13:59:57 +0800

    feat: Answers 09_palindromes Answers and passes all the tests of the 09_palindromes folder.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    // \W matches any non-word character, including spaces. _ is technically alphanumeric
+    // Regexes are enclosed in slashes than quotes in js
+    const cleaned = word.toLowerCase().replace(/[\W_]/g, "");
+    const reversed = cleaned.split("").reverse().join("");
+    return cleaned === reversed;
 };
 
 // Do not edit below this line
